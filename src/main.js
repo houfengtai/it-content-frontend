fb4f863ae45317ae4d830c806fee0e6b5e569539
houfengtai <houfengtai@sumawuliu.com>
2019-08-27 17:58:15 +0800

    init pro

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './vuex/store'
+import MetaInfo from 'vue-meta-info'
 
+Vue.use(MetaInfo)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
